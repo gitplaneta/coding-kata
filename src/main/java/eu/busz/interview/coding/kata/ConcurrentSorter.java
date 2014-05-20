4f79802f4aa2503f97b471b7bae0073c74344116
Radoslaw Busz <radoslaw.busz@gmail.com>
2014-05-20 21:17:07 +0200

    First phase tests to be implemented

---
+++ src/main/java/eu/busz/interview/coding/kata/ConcurrentSorter.java
@@ -0,0 +1,13 @@
+package eu.busz.interview.coding.kata;
+
+import java.util.List;
+
+/**
+ *
+ * @author Radoslaw Busz <radoslaw.busz at gmail.com>
+ */
+public class ConcurrentSorter  {
+    public <T extends Comparable<T>> List<T> sort(List<T> input) {
+        return null;
+    }
+}
--- src/test/java/eu/busz/kata/forkjoin/mergesort/SortingAlgorithmTest.java
+++ src/test/java/eu/busz/kata/forkjoin/mergesort/SortingAlgorithmTest.java
@@ -1,8 +1,11 @@
 package eu.busz.kata.forkjoin.mergesort;
 
+import eu.busz.interview.coding.kata.ConcurrentSorter;
+import java.util.ArrayList;
+import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *
@@ -11,7 +14,26 @@ import static org.junit.Assert.*;
 public class SortingAlgorithmTest {
 
     @Test
-    public void test() {
-        assertThat(true, equalTo(true));
+    public void sortsSingleElementCollection() {
+        List<Integer> input = toList(5);
+
+        ConcurrentSorter cs = new ConcurrentSorter();
+        assertThat("single element collection sort", cs.sort(input), equalTo(toList(5)));
+    }
+
+    @Test
+    public void sortsEmptyCollection() {
+        List<Integer> input = new ArrayList<>(0);
+        ConcurrentSorter cs = new ConcurrentSorter();
+
+        assertThat("returns empty collection when sorting empty", cs.sort(input), equalTo(toList()));
+    }
+
+    private List<Integer> toList(int... elements) {
+        List<Integer> result = new ArrayList<>();
+        for (int element : elements) {
+            result.add(element);
+        }
+        return result;
     }
 }
