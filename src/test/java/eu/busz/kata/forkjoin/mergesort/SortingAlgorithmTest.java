2417cf9679d36386e9e7908b733f83accf2ae00b
unknown <radoslaw.busz@gmail.com>
2014-05-20 20:53:00 +0200

    Init commit

---
+++ src/test/java/eu/busz/kata/forkjoin/mergesort/SortingAlgorithmTest.java
@@ -0,0 +1,17 @@
+package eu.busz.kata.forkjoin.mergesort;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Radoslaw Busz <radoslaw.busz at gmail.com>
+ */
+public class SortingAlgorithmTest {
+
+    @Test
+    public void test() {
+        assertThat(true, equalTo(true));
+    }
+}
