9f6d828193a14a79d59e2df550b94eb7763f4ee7
Radoslaw Busz <radoslaw.busz@espeo.pl>
2014-06-10 16:02:08 +0200

    initial mergesort algo

--- src/main/java/eu/busz/interview/coding/kata/ConcurrentSorter.java
+++ src/main/java/eu/busz/interview/coding/kata/ConcurrentSorter.java
@@ -2,12 +2,41 @@ package eu.busz.interview.coding.kata;
 
 import java.util.List;
 
-/**
- *
- * @author Radoslaw Busz <radoslaw.busz at gmail.com>
- */
-public class ConcurrentSorter  {
-    public <T extends Comparable<T>> List<T> sort(List<T> input) {
-        return null;
+public class ConcurrentSorter<T extends Comparable<T>> {
+
+    public T[] sort(T[] input) {
+        mergeSort(input, 0, input.length);
+
+        return input;
+    }
+
+    private void mergeSort(T[] arr, int begin, int end) {
+        if (begin >= end) {
+            mergeSort(arr, begin, end / 2);
+            mergeSort(arr, end / 2 + 1, end);
+
+            merge(arr, begin, end);
+        }
+    }
+
+    private void merge(T[] arr, int begin, int end) {
+        int mid = end / 2;
+        int right = begin;
+        int left = end / 2 + 1;
+
+        if (arr[left].compareTo(arr[mid]) <= 0) {
+            left++;
+        } else {
+            while (right < mid && left < end) {
+                T tmp = arr[mid];
+
+            }
+        }
+    }
+
+    private <T> void swap(int a, int b, T[] arr) {
+        T tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
     }
 }
--- src/test/java/eu/busz/kata/forkjoin/mergesort/SortingAlgorithmTest.java
+++ src/test/java/eu/busz/kata/forkjoin/mergesort/SortingAlgorithmTest.java
@@ -1,8 +1,6 @@
 package eu.busz.kata.forkjoin.mergesort;
 
 import eu.busz.interview.coding.kata.ConcurrentSorter;
-import java.util.ArrayList;
-import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -15,25 +13,33 @@ public class SortingAlgorithmTest {
 
     @Test
     public void sortsSingleElementCollection() {
-        List<Integer> input = toList(5);
+        Integer[] input = toArray(5);
 
         ConcurrentSorter cs = new ConcurrentSorter();
-        assertThat("single element collection sort", cs.sort(input), equalTo(toList(5)));
+        assertThat("single element collection sort", cs.sort(input), equalTo(toArray(5)));
     }
 
     @Test
     public void sortsEmptyCollection() {
-        List<Integer> input = new ArrayList<>(0);
+        Integer[] input = new Integer[0];
         ConcurrentSorter cs = new ConcurrentSorter();
 
-        assertThat("returns empty collection when sorting empty", cs.sort(input), equalTo(toList()));
+        assertThat("returns empty collection when sorting empty", cs.sort(input), equalTo(toArray()));
     }
 
-    private List<Integer> toList(int... elements) {
-        List<Integer> result = new ArrayList<>();
-        for (int element : elements) {
-            result.add(element);
-        }
-        return result;
+    @Test
+    public void sortsFiveElementList() {
+        Integer[] input = toArray(5,11,-1,0,0);
+        ConcurrentSorter cs = new ConcurrentSorter();
+
+        assertThat("retuns sorted list", cs.sort(input), equalTo(toArray(-1,0,0,5,11)));
+    }
+
+    private Integer[] toArray(Integer... elements) {
+//        List<Integer> result = new ArrayList<>();
+//        for (int element : elements) {
+//            result.add(element);
+//        }
+        return elements;
     }
 }
