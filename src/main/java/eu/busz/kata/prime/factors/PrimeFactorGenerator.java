c4700d684c3aa73474e6440ad3ebe9aa9a12d03c
Radoslaw Busz <radoslaw.busz@espeo.pl>
2015-05-22 16:02:50 +0200

    One value has no prime factors

--- src/main/java/eu/busz/kata/prime/factors/PrimeFactorGenerator.java
+++ src/main/java/eu/busz/kata/prime/factors/PrimeFactorGenerator.java
@@ -3,11 +3,16 @@ package eu.busz.kata.prime.factors;
 import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PrimeFactorGenerator {
 
     public static List<Integer> generatePrimeFactors(int forValue) {
+        if (forValue == 1) {
+            return Collections.emptyList();
+        }
+
         return ImmutableList.of(forValue);
     }
 }
--- src/test/java/eu/busz/kata/prime/factors/PrimeFactorTest.java
+++ src/test/java/eu/busz/kata/prime/factors/PrimeFactorTest.java
@@ -3,9 +3,11 @@ package eu.busz.kata.prime.factors;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static eu.busz.kata.prime.factors.PrimeFactorGenerator.generatePrimeFactors;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -15,5 +17,10 @@ public class PrimeFactorTest {
     public void forPrimeNumberReturnsTheSameValue() {
         List<Integer> result = generatePrimeFactors(2);
         assertThat("Prime numbers have only one prime factor", result, equalTo(ImmutableList.of(2)));
+    }
+
+    @Test
+    public void valueOfOneHasNoPrimeFactors() {
+        assertThat("One has no prime factors", generatePrimeFactors(1), equalTo(emptyList()));
     }
 }
